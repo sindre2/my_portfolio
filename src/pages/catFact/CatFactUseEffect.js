1b69c5c95ca30682428b62cc861635beec0390e0
sindre2 <sindre2.kodehode@gmail.com>
2022-09-16 11:26:26 +0200

    update

--- src/pages/catFact/CatFactUseEffect.js
+++ src/pages/catFact/CatFactUseEffect.js
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
 
 export function CatFactUseEffect() {
-  const [web, setWeb] = useState("Hello");
-  const [count, setCount] = useState(0);
+  const [web, setWeb] = useState();
 
   useEffect(() => {
     fetch("https://catfact.ninja/facts")
       .then((response) => response.json())
       .then((info) => {
         setWeb(info.data);
-        console.log(info);
-          return (
-            <>
-              <h3>{web[0].fact}</h3>
-            </>
-          );
       });
-  }, [count]);
+  }, []);
 
-  setTimeout(() => {
-    setCount((count) => count + 1);
-  }, 4000);
+  if (web) {
+    return (
+      <>
+        {web.map((item, idx) => {
+          return <h3 key={idx}>{item.fact}</h3>;
+        })}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h3>No facts yet</h3>
+      </>
+    );
+  }
 }
